e3d2e8c29cc6311195a8b8238f6e3de3bc9b35be
Noah <noah@coronasoftware.net>
2022-03-29 23:20:14 -0700

    Include readme in crate docs

--- crates/yewdux/src/lib.rs
+++ crates/yewdux/src/lib.rs
@@ -1,13 +1,4 @@
-//! # Yewdux
-//!
-//! Shared state containers for [Yew](https://yew.rs/docs/en/) applications.
-//!
-//! State management in Yew can get cumbersome, especially when you need to give many (potentially
-//! isolated) components mutable access to shared state. Normally you would need to write individual
-//! properties and callbacks for each component to propagate changes -- too much typing if you as me!
-//! Yewdux provides an ergonomic interface for shared state containers. They can be accessed from any
-//! component or agent, live for entire application lifetime, and are clone-on-write by default.
-//!
+#![doc = include_str!("../../../README.md")]
 #![allow(clippy::needless_doctest_main)]
 
 // pub mod component;
