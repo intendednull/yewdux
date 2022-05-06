8a8c5d746f2fdc06ba6fc36a8d7f12313f753a29
Noah <noah@coronasoftware.net>
2022-05-06 01:10:20 -0700

    Add listener example

---
+++ examples/listener/src/main.rs
@@ -0,0 +1,50 @@
+use std::rc::Rc;
+
+use yew::prelude::*;
+use yewdux::{prelude::*, storage};
+
+use serde::{Deserialize, Serialize};
+
+struct StorageListener;
+impl Listener for StorageListener {
+    type Store = State;
+
+    fn on_change(&mut self, state: Rc<Self::Store>) {
+        if let Err(err) = storage::save(state.as_ref(), storage::Area::Local) {
+            println!("Error saving state to storage: {:?}", err);
+        }
+    }
+}
+
+#[derive(Default, Clone, PartialEq, Deserialize, Serialize)]
+struct State {
+    count: u32,
+}
+
+impl Store for State {
+    fn new() -> Self {
+        init_listener(StorageListener);
+
+        storage::load(storage::Area::Local)
+            .ok()
+            .flatten()
+            .unwrap_or_default()
+    }
+}
+
+#[function_component]
+fn App() -> Html {
+    let (state, dispatch) = use_store::<State>();
+    let onclick = dispatch.reduce_mut_callback(|state| state.count += 1);
+
+    html! {
+        <>
+        <p>{ state.count }</p>
+        <button {onclick}>{"+1"}</button>
+        </>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
