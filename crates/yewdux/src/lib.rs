0d420c5929ef1a70dece986af06ba3bcfdedf398
Noah <noah@coronasoftware.net>
2022-08-18 18:24:52 -0700

    Fix release items

--- crates/yewdux/src/lib.rs
+++ crates/yewdux/src/lib.rs
@@ -1,4 +1,11 @@
-#![doc = include_str!("../../../README.md")]
+//! # Yewdux
+//!
+//! Simple state management for [Yew](https://yew.rs) applications.
+//!
+//! This is the development branch. Latest stable release may be found
+//! [here](https://github.com/intendednull/yewdux/tree/0.7.0).
+//!
+//! See the [book](https://intendednull.github.io/yewdux/) for more details.
 #![allow(clippy::needless_doctest_main)]
 
 // pub mod component;
